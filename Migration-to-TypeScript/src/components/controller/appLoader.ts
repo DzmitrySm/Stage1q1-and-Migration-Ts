3405ca218ffe4b3f657a566e01d7d6d2f4090894
DzmitrySm <magicjoker@yandex.ru>
2022-07-05 03:38:38 +0300

    refactor: change api link

--- Migration-to-TypeScript/src/components/controller/appLoader.ts
+++ Migration-to-TypeScript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '1adcca06e463459899c6f272d67c3608', // получите свой ключ https://newsapi.org/
         });
     }
